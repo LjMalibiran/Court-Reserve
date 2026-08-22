bb6b23143842048ecfe43ed101c7be9adaaaed3d
143LJMALIBIRAN@gmail.com <143LJMALIBIRAN@gmail.com>
2026-08-23 00:10:56 +0800

    Fix CSS loading with HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- ADD THIS LINE
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Force HTTPS if the website is live
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
